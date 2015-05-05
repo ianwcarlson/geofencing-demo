7f3a9b558fb314390bb80f1ec68c81d9fc41c16c
ianwcarlson <ianwcarlson@gmail.com>
2015-05-05 17:40:15 -0600

    still piecing together all the dependencies and modifying the config to reflect new paths

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static(__dirname + '../client'));
+app.use(express.static(__dirname + '../bower_compenents/leaflet.draw/dist'));
 
 io.on('connection', function(socket){
 	socket.on('newPolygonPoints', function(newPolygonPoints){			
